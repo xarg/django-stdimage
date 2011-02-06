409a1da2341273df4f66e7eac6fca44af6dc9466
Alexandru Plugaru <alexandru.plugaru@gmail.com>
2011-02-06 15:36:25 +0200

    Fixes issue #5

--- stdimage/widgets.py
+++ stdimage/widgets.py
@@ -18,7 +18,8 @@ class DelAdminFileWidget(AdminFileWidget):
             output.append('<table style="border-style: none;">')
             output.append(item % (_('Currently:'), '<a target="_blank" href="%s%s">%s</a>' % (settings.MEDIA_URL, value, value)))
             output.append(item % (_('Change:'), input))
-            output.append(item % (_('Delete') + ':', '<input type="checkbox" name="%s_delete"/>' % name)) # split colon to force "Delete" that is already translated
+            if value.field.blank is True:
+                output.append(item % (_('Delete') + ':', '<input type="checkbox" name="%s_delete"/>' % name)) # split colon to force "Delete" that is already translated
             output.append('</table>')
             return mark_safe(u''.join(output))
         else:
@@ -29,4 +30,3 @@ class DelAdminFileWidget(AdminFileWidget):
             return super(DelAdminFileWidget, self).value_from_datadict(data, files, name)
         else:
             return '__deleted__'
-
